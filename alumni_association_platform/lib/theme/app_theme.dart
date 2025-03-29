a17e95586523cf6fcc350c19ccb518356c009a02
Kiran Surya <146125805+KiranSurya-SU@users.noreply.github.com>
2025-03-29 19:58:43 +0530

    Add alumni_association_platform/lib/theme/app_theme.dart 1743258526216

---
+++ alumni_association_platform/lib/theme/app_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.indigo,
+    scaffoldBackgroundColor: Colors.grey[50],
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.indigo,
+      foregroundColor: Colors.white,
+    ),
+    cardTheme: CardTheme(
+      elevation: 2,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.indigo,
+    scaffoldBackgroundColor: Colors.grey[900],
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.grey[850],
+    ),
+    cardTheme: CardTheme(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      color: Colors.grey[850],
+    ),
+  );
+}
