7c32cb82019c8efcc0f83b9a03d378a39dfa5209
Kiran Surya <146125805+KiranSurya-SU@users.noreply.github.com>
2025-03-29 19:57:36 +0530

    Add alumni_association_platform/lib/firebase_options.dart 1743258459403

---
+++ alumni_association_platform/lib/firebase_options.dart
@@ -0,0 +1,33 @@
+// NOTE: This file will be generated after setting up Firebase project
+// and running `flutterfire configure` command
+//
+// For now, we'll create a placeholder that will be replaced with
+// actual configuration when we set up Firebase
+
+class DefaultFirebaseOptions {
+  static const FirebaseOptions web = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+  );
+
+  static const FirebaseOptions android = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+  );
+
+  static const FirebaseOptions ios = FirebaseOptions(
+    apiKey: 'YOUR_API_KEY',
+    appId: 'YOUR_APP_ID',
+    messagingSenderId: 'YOUR_SENDER_ID',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    iosBundleId: 'YOUR_BUNDLE_ID',
+  );
+}
